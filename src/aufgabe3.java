832caa6dbf007c0793d22e5166d087e19ace9733
SergiuCampeanu <sncampeanu@gmail.com>
2021-10-08 22:30:26 +0300

    m

--- src/aufgabe3.java
+++ src/aufgabe3.java
@@ -1,2 +1,88 @@
+/*
+import java.util.*;
 public class aufgabe3 {
+    public static void main(String[] args) {
+
+        int[] Number1 = {1, 4, 0, 0, 0, 0, 7, 0, 0};
+        int[] Number2 = {8, 7, 0, 0, 0, 0, 7, 0, 0};
+
+        System.out.println("Addition: " + add(Number1, Number2));
+
+    }
+
+    private static ArrayList<Integer> add(int[] arr1, int[] arr2) {
+        ArrayList<Integer> num3 = new ArrayList<Integer>();
+       // int[] num3 = new int[arr1.length];
+        int next=0;
+        for (int i = arr1.length-1; i > 0; i--)
+        {
+            if(arr1[i] + arr2[i]+ next > 9)
+            {
+                num3.add((arr1[i]+arr2[i]+next)%10);
+                next=(arr1[i]+arr2[i]+next)/10;
+                System.out.println(next);  //debug
+*/
+/*                if( == null)
+                {
+
+                    num3 = Arrays.copyOf(num3,num3.length + 1); //resize array-ului
+                }*//*
+
+            }
+            else
+            {
+                num3.add(arr1[i]+arr2[i]+next);
+                next=0;
+            }
+        }
+        return num3;
+    }
+
+}*/
+
+
+import java.util.*;
+public class aufgabe3 {
+    public static void main(String[] args) {
+
+        int[] Number1 = {1, 4, 0, 0, 0, 0, 7, 0, 0};
+        List<Integer> intList1 = new ArrayList<Integer>(Number1.length);
+        for (int i : Number1)
+        {
+            intList1.add(i);
+        }
+
+        int[] Number2 = {8, 7, 0, 0, 0, 0, 7, 0, 0};
+        List<Integer> intList2 = new ArrayList<Integer>(Number2.length);
+        for (int i : Number2)
+        {
+            intList2.add(i);
+        }
+
+
+        System.out.println("Addition: " + add(intList1, intList2));
+
+    }
+
+    private static ArrayList<Integer> add( List<Integer> arr1,  List<Integer> arr2) {
+        ArrayList<Integer> num3 = new ArrayList<Integer>();
+        // int[] num3 = new int[arr1.length];
+        int next=0;
+        for (int i = arr1.size()-1; i > 0; i--)
+        {
+            if(arr1.get(i) + arr2.get(i)+ next > 9)
+            {
+                num3.add((arr1.get(i) + arr2.get(i)+ next)%10);
+                next=(arr1.get(i) + arr2.get(i)+ next)/10;
+                System.out.println(next);  //debug
+            }
+            else
+            {
+                num3.add(arr1.get(i) + arr2.get(i)+ next);
+                next=0;
+            }
+        }
+        return num3;
+    }
+
 }
--- src/aufgabe4.java
+++ src/aufgabe4.java
@@ -1,2 +1,113 @@
+import java.util.*;
+import java.util.Scanner;
 public class aufgabe4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] KeyBoardPrice = {40, 35, 70, 15, 45};
+        int[] USBCablePrice = {20, 15, 40, 75};
+        System.out.println("Hello to the Store!");
+        System.out.println("KeyBoard's Price" + Arrays.toString(KeyBoardPrice));
+        System.out.println("USB Cable's Price" + Arrays.toString(USBCablePrice));
+        System.out.println(" "); //new line
+
+        System.out.println("Cheapest Keyboard: " + CheapKeyBoard(KeyBoardPrice));
+        System.out.println("The most expensive Keyboard: " + ExpKeyBoard(KeyBoardPrice)+", and the most expensive USB Cable: " + ExpUSBCable(USBCablePrice));
+        System.out.println(" ");
+
+        System.out.print("Enter Markus's Budget: ");
+        int budget = scan.nextInt();
+        System.out.println("The most expensive USB Cable that Markus can afford is: " + MarkusUSBEXP(USBCablePrice, budget));
+        System.out.println("The best set up that Markus can afford is: " + BestSetUP(KeyBoardPrice, USBCablePrice, budget));
+
+    }
+
+
+    private static int CheapKeyBoard(int[] arr) {
+        int ChKB = arr[0];
+        for (int x : arr) {
+            if (x < ChKB) {
+                ChKB = x;
+            }
+        }
+        return ChKB;
+    }
+
+    private static int ExpKeyBoard(int[] arr) {
+        int exp = arr[0];
+        for (int x : arr) {
+            if (x > exp) {
+                exp = x;
+            }
+        }
+        return exp;
+    }
+
+    private static int ExpUSBCable(int[] arr) {
+        int exp = arr[0];
+        for (int x : arr) {
+            if (x > exp) {
+                exp = x;
+            }
+        }
+        return exp;
+    }
+
+    private static int MarkusUSBEXP(int[] arr, int budget) {
+        int exp = arr[0];
+        boolean found = false;
+        for (int x : arr) {
+            if(x<budget)
+            {
+                if (x > exp) {
+                    exp = x;
+                    found = true;
+                }
+            }
+        }
+        if (!found) //found == false
+        {
+            return -1;
+        }
+        else
+        {
+            return exp;
+        }
+    }
+
+    private static Object BestSetUP(int[] arr1, int[] arr2, int budget)
+    {
+        int sum = 0;
+        ArrayList<Integer> bsp = new ArrayList<Integer>();
+        boolean found = false;
+        int expK = arr1[0];
+        int expU = arr2[0];
+        for (int i : arr1)
+        {
+            for (int j : arr2)
+            {
+                if (i + j <= budget)
+                {
+                    if (i + j > sum)
+                    {
+                        sum = i + j;
+                        found = true;
+                        bsp.clear();
+                        bsp.add(i);
+                        bsp.add(j);
+                    }
+                }
+            }
+        }
+        if (!found) //found == false
+        {
+            return -1;
+        }
+        else
+        {
+            return bsp;
+        }
+    }
+
 }
